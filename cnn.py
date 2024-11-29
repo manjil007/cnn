eca31ff9b99b9d2bdee1e269fc56f4cc0db5a176
shahriar1489 <shahriar294@gmail.com>
2024-11-29 11:29:31 -0700

    added function definitions

--- cnn.py
+++ cnn.py
@@ -3,15 +3,38 @@ import scipy.signal as signal
 
 class cnn:
     def __init__(self, input_size, num_kernels, kernel_size):
+        """
+        Constructor for the cnn class. Initializes key attributes for the CNN: 
+            - input_size: Size of the input image 
+            - num_kernels : Number of convolutional filters to use
+            - kernel_size : Size of the convolutional kernel/filter  
+        """  
         self.input_size = input_size
         self.kernel_size = kernel_size
         self.num_kernels = num_kernels
 
     def __repr__(self):
+        """
+        Summarizes the initialized CNN object. 
+        """
+        
+        
         return (f"cnn(num_input={self.num_input}, num_output={self.num_output}, "
                 f"kernel_size={self.kernel_size}, stride={self.stride}, padding={self.padding})")
     
     def generate_cnn_patches(self, image, stride, padding):
+        """
+        
+        Extracts sub-regions or small patches from the input image that the convolution operation will use. 
+        Steps: 
+            1. kernel_height (kernel_h) and width (kernel_w) based on the given kernel_size. 
+            2. pads image with 0 if padding is specified 
+            3. loops over the image with the specified stride, extracting kernel_{h} x kernel_{w} patches for each channel 
+            4. returns a collection of these patches 
+        
+        """
+        
+        
         num_channel, image_h, image_w = image.shape
 
         # Determine kernel size
@@ -43,14 +66,27 @@ class cnn:
             channel_patch = np.array(channel_patch)
             patches.append(channel_patch)
         patches = np.array(patches)
+        
         return patches
     
     def convolute(self, patch, kernel):
+        """
+        Performs the convolution operation between a single patch and a kernel 
+        Steps: 
+            1. Uses scipy.signal.correlate2d to compute the valid cross-correlation between the patch and kernel 
+            2. Returns the resulting matrix
+        """
+        
         conv_mat = signal.correlate2d(patch, kernel, mode='valid')
+        
         return conv_mat
-
     
     def convolve(self, img, stride, padding):
+        """
+        The core function that orchestrates the convolution process for the entire image for the entire image  
+        """
+        
+        
         patches = self.generate_cnn_patches(img, stride, padding)
         num_channels, input_h, input_w = img.shape ##need to indexing value for right shape
         self.output_shape = (self.num_kernels, input_h - self.kernel_size + 1, input_w - self.kernel_size + 1)
@@ -77,16 +113,28 @@ class cnn:
         
 
         return self.output
-    
-    def max_pooling(self, convolved_img, stride)
 
-                            
+    def MaxPool(kernel_size , stride=1): 
+        """
+        
+        """
+        
+    def MinPool(kernel_size , stride =1): 
+        """
 
+        """
+    def AvgPool(kernel_size , stride = 1): 
+        """
+        
+        """
 
+    #max_pooling(self, convolved_img, stride)               
 
 
         
-        
+    #def max_pooling(self, convolved_img, stride)
+
+                                
 
 
     # def generate_cnn_patches(self, image):
---
+++ pool.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import sys 
+import os 
+
+class pool: 
+    def __init__(self, convolved image, stride ,type="max"): 
+        """
+        The constructor of the pooling layer.asd
+        """
+        # incomplete 
+
+    def MaxPool(kernel_size , stride=1): 
+
+    def MinPool(kernel_size , stride =1): 
+
+    def AvgPool(kernel_size , stride = 1): 
+
+
+    #max_pooling(self, convolved_img, stride)       
