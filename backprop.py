4499a413c4ca7d20b7a7e33197f115345f1c92e3
shahriar1489 <shahriar294@gmail.com>
2024-12-08 14:10:25 -0700

    revised dL_dx and pooling layer with batch

--- backprop.py
+++ backprop.py
@@ -7,7 +7,7 @@ import os
 So far: There is only one pooling layer that will pool from each channel 
 """
 
-def backward_max_pool(pooled_image): 
+def backward_max_pool(image_batch, pooling_layers): 
     """
     
     Description: 
@@ -16,8 +16,7 @@ def backward_max_pool(pooled_image):
 
     Parameters: 
     -----------
-    - pooled_layer : For a single 3D channel, it is the output of the pooling layer. Say, it's shape is HxW  
-    
+    - pooled_layer : The batch of input image. 
     
     Returns : 
     ---------
@@ -25,6 +24,153 @@ def backward_max_pool(pooled_image):
     
     """
 
+
+
+
     
-    dL_wrt_pooling_layer  = (pooled_layer > 0).astype(int) 
+    dL_wrt_pooling_layer  = (pooled_layers > 0).astype(int) 
     return dL_wrt_pooling_layer
+
+
+"""
+
+- generate dummy dL_dz 
+- generate dummy output 
+
+
+"""
+
+dL_dz_test = np.random.rand(6, 28, 28)
+z = np.random.rand( 6, 28, 28) 
+kernel = np.random.rand(6,5,5)
+
+# revised 
+def backward_wrt_input(dL_dz, kernel ,stride=1):     
+    """
+    Description: 
+    -----------
+    The image input is an input of dimension (1, H, W). 
+    KEY INSIGHT : The CNN Backpropagation operation with stride>1 is identical to a stride = 1. 
+    
+    Parameters: 
+    -----------
+        dL_dz  : Partial derivative of Loss wrt the kernel
+        kernel : The kernel/filter of the current layer 
+        stride : Although defined, the CNN backpropagation operation with stride >1  is identical to stride=1. Source: https://deeplearning.cs.cmu.edu/F21/document/recitation/Recitation5/CNN_Backprop_Recitation_5_F21.pdf
+
+    Returns: 
+    --------
+        The partial derivative dL_dx 
+    ----
+    
+    Note: 
+        i)  See if stride is a factor here. ->> The CNN backpropagation operation with stride >1  is identical to stride=1. Source: https://deeplearning.cs.cmu.edu/F21/document/recitation/Recitation5/CNN_Backprop_Recitation_5_F21.pdf 
+        ii) The weight update will happen outside this function
+        iii) 
+        
+        key formula : dL_dx = full_convolution(kernel, loss gradient dL_dz ) 
+        dL_dO is dL_dz in MMP's code
+
+        z/O is the output of the convolution layer 
+        dL_do is same as dL_dz 
+    
+    ----
+    
+    """    
+    
+    print("dL_dz shape ", dL_dz.shape) # the loss should be a
+    
+
+    #sys.exit(-1) 
+    # Since this is a single layer, let us ignore the dimension C.
+    
+    # Step 1. filp the filter/kernel 180 degree; assume that a single kernel is 3D i.e. (1, H, W) 
+    kernel_rotated = kernel[: , ::-1, ::-1 ] # replace this with
+    
+    # Step 2. pad the dL_dz
+    dL_dz_padded_1 = padding(dL_dz_test, 1) # padding dL_dZ with 1) 
+
+    """
+    
+    dL_dz_test_padded_1 shape : (1, 30, 30) 
+    kernel_rotated : (1, 5, 5) 
+    
+    convolutional arithmetic : ( ((30-5) + 2* padding) / stride) ) + 1  
+        
+    """
+
+    image_height = dL_dz_padded_1.shape[-1]
+    kernel_height = kernel_rotated.shape[-1]
+    stride_ = stride   
+
+
+    #print("---------") 
+    
+    #print("dz_dz after padding, length : ", image_height) 
+    #print("kernel height, length : ", kernel_height) 
+    #print("stride, length : ", stride_) 
+    
+    output_dimension = (image_height - kernel_height // stride) + 1
+
+    #print("output dimension : ", output_dimension)
+
+    #print("---------") 
+    #sys.exit(-1) 
+    
+    #print("dL_dz before padding : " , dL_dz_test.shape ) 
+    #print("dL_dz_padded_1 , after initialization : ", dL_dz_padded_1.shape ) 
+
+    #print("Update : Padding applied successfully" ) 
+    output = np.zeros( (output_dimension, output_dimension) ) 
+    print("output shape ", output.shape) 
+    
+    #sys.exit(-2) 
+    
+    # Step 3: 
+    # perform convolution between dL_dz_padded_1 and kernel_rotated- the results will be in output matrix 
+    
+    print("--------------") 
+    # Step 3: Perform the convolution operation --->> This should be a separate function., 
+    
+    # add another loop to iterate across all the channels 
+    # (Batch Size, Channel, Height, Width) --> in my code (Channel (C), Height (i), Width (j)) 
+    
+    for i in range(0, output_dimension):
+        
+        for j in range(0, output_dimension):
+            
+            # Extract the patch of the same size as the kernel
+            patch = dL_dz_padded_1[0, i:i+kernel_height, j:j+kernel_height] 
+
+            # Perform element-wise multiplication and sum
+            output[i, j] = np.sum(patch * kernel_rotated[0])
+
+            print("i, j: (", i, j , ")")
+
+
+     
+    
+    #print("dL_dx: After convolution is applied")
+    #print(output)
+    print("--------------") 
+
+
+    """
+    output is dL/dX 
+    """
+    #for i in range(dl_dz_padded_1.shape[0]): #6, 32, 32 #x = 32, 32     
+    #    convolve(dl_dz_padded[i], kernel)
+                                 
+        #    output[] = convolute(patch, kernel)
+
+    #print("dL_dx : after convolution is applied", ) 
+
+    #print("output " , output.shape) 
+
+    return output # this is dL/dx
+         
+
+#backward_wrt_input(dL_dz_test, kernel, stride=1)    
+
+
+
--- utils_cnn.py
+++ utils_cnn.py
@@ -2,7 +2,6 @@
 This file contains utility functions used in cnn operation. 
     - padding
 """
-
 import numpy as np 
 import sys 
 import os 
@@ -25,7 +24,6 @@ def padding(image, pad):
         The amount of zero-padding to add around the borders on each channels.
         Padding ill be added symmetrically so that there is equal padding on all sides.
 
-
     Returns:
     --------
 
@@ -45,29 +43,32 @@ def padding(image, pad):
                                        mode='constant', 
                                        constant_values=0)
     return output
-
-
+    
+# revised 
 def MaxPool( conv_image , kernel_size , stride=1): 
         """
         Subsampling layer. 
 
         Parameters:
         -----------
-            - conv_image : image in numpy and dimension (C, H, W) 
+            - conv_image : image in numpy and dimension (B, C, H, W) 
+                - B is the batch size 
+                - C is the channel 
+                - H is the height 
+                - W is the wieght 
+                
             - kernel_size : kernel_size of the pooling layer. The kernel stays rectangular  
             - stride : stide of the pooling layer 
 
         Returns:
         --------
-        pooled in numpy 
+        pooled batch in numpy 
         
         """
-        # Get input dimensions 
-        input_channel ,input_height , input_width = conv_image.shape
+        batch_size, input_channel ,input_height , input_width = conv_image.shape
     
-        # Get the output dimensions 
-        output_height = (input_height - kernel_size) // stride+1 
-        output_width = (input_width - kernel_size) // stride+1 
+        output_height = ( (input_height - kernel_size) // stride) +1 
+        output_width = ( (input_width - kernel_size) // stride ) +1 
 
         """
         print("input height : " , input_height ) 
@@ -76,34 +77,152 @@ def MaxPool( conv_image , kernel_size , stride=1):
         print("output_width : ", output_width ) 
         """
         
-    
-        # initialize a np 3D array for pooling kernel 
-        pooled_ = np.zeros((input_channel, output_height, output_width )) 
-        #print(pooled_.shape) 
-
-        #sys.exit(-1) 
+        pooled = np.zeros( (batch_size, input_channel, output_height, output_width) ) 
             
         # 2. Scan through the input image and get the max 
 
-        # The iteration below will be honsistent for 3 dimensions; as of now it is for 2 . 
-        # Apply pooling operation per channel
-    
-        for c in range(input_channel): # iterate across the channel 
+        """"
+        Applying the pooling per channel in batch 
+        """"
+        
+        for b in range(batch_size):  # iterate over batch
             
-            for j in range(0, input_height - kernel_size + 1, stride): # iterate across the height
+            for c in range(input_channel):  # iterate over channels
                 
-                for i in range(0, input_width - kernel_size + 1, stride): # iterate across the width 
+                for j in range(0, input_height - kernel_size + 1, stride):  # height
                     
-                    # Define the current window
-                    
-                    window = conv_image[c, j:j + kernel_size, i:i + kernel_size]
-        
-                    # Get the max value in the window
-                    max_value = np.max(window)
-        
-                    # Map max value to the output matrix
-                    pooled_[c, j // stride, i // stride] = max_value # fixed to get integer size
-        
-        return pooled_
+                    for i in range(0, input_width - kernel_size + 1, stride):  # width
+                        
+                        window = conv_image[b, c, j:j + kernel_size, i:i + kernel_size] # window for the current iteration 
 
-# Get the backprop wrt to the input 
+                        max_value = np.max(window) # max value from the current window 
+
+                        pooled[b, c, j // stride, i // stride] = max_value # Store the max value in the output array
+
+        return pooled
+
+# revised 
+def backward_wrt_input(dL_dz, kernel ,stride=1):     
+    """
+    Description: 
+    -----------
+    The image input is an input of dimension (1, H, W). 
+    KEY INSIGHT : The CNN Backpropagation operation with stride>1 is identical to a stride = 1. 
+    
+    Parameters: 
+    -----------
+        dL_dz  : Partial derivative of Loss wrt the kernel
+        kernel : The kernel/filter of the current layer 
+        stride : Although defined, the CNN backpropagation operation with stride >1  is identical to stride=1. Source: https://deeplearning.cs.cmu.edu/F21/document/recitation/Recitation5/CNN_Backprop_Recitation_5_F21.pdf
+
+    Returns: 
+    --------
+        The partial derivative dL_dx 
+    ----
+    
+    Note: 
+        i)  See if stride is a factor here. ->> The CNN backpropagation operation with stride >1  is identical to stride=1. Source: https://deeplearning.cs.cmu.edu/F21/document/recitation/Recitation5/CNN_Backprop_Recitation_5_F21.pdf 
+        ii) The weight update will happen outside this function
+        iii) 
+        
+        key formula : dL_dx = full_convolution(kernel, loss gradient dL_dz ) 
+        dL_dO is dL_dz in MMP's code
+
+        z/O is the output of the convolution layer 
+        dL_do is same as dL_dz 
+    
+    ----
+    
+    """    
+    
+    print("dL_dz shape ", dL_dz.shape) # the loss should be a
+    
+
+    #sys.exit(-1) 
+    # Since this is a single layer, let us ignore the dimension C.
+    
+    # Step 1. filp the filter/kernel 180 degree; assume that a single kernel is 3D i.e. (1, H, W) 
+    kernel_rotated = kernel[: , ::-1, ::-1 ] # replace this with
+    
+    # Step 2. pad the dL_dz
+    dL_dz_padded_1 = padding(dL_dz_test, 1) # padding dL_dZ with 1) 
+
+    """
+    
+    dL_dz_test_padded_1 shape : (1, 30, 30) 
+    kernel_rotated : (1, 5, 5) 
+    
+    convolutional arithmetic : ( ((30-5) + 2* padding) / stride) ) + 1  
+        
+    """
+
+    image_height = dL_dz_padded_1.shape[-1]
+    kernel_height = kernel_rotated.shape[-1]
+    stride_ = stride   
+
+
+    #print("---------") 
+    
+    #print("dz_dz after padding, length : ", image_height) 
+    #print("kernel height, length : ", kernel_height) 
+    #print("stride, length : ", stride_) 
+    
+    output_dimension = (image_height - kernel_height // stride) + 1
+
+    #print("output dimension : ", output_dimension)
+
+    #print("---------") 
+    #sys.exit(-1) 
+    
+    #print("dL_dz before padding : " , dL_dz_test.shape ) 
+    #print("dL_dz_padded_1 , after initialization : ", dL_dz_padded_1.shape ) 
+
+    #print("Update : Padding applied successfully" ) 
+    output = np.zeros( (output_dimension, output_dimension) ) 
+    print("output shape ", output.shape) 
+    
+    #sys.exit(-2) 
+    
+    # Step 3: 
+    # perform convolution between dL_dz_padded_1 and kernel_rotated- the results will be in output matrix 
+    
+    print("--------------") 
+    # Step 3: Perform the convolution operation --->> This should be a separate function., 
+    
+    # add another loop to iterate across all the channels 
+    # (Batch Size, Channel, Height, Width) --> in my code (Channel (C), Height (i), Width (j)) 
+    
+    for i in range(0, output_dimension):
+        
+        for j in range(0, output_dimension):
+            
+            # Extract the patch of the same size as the kernel
+            patch = dL_dz_padded_1[0, i:i+kernel_height, j:j+kernel_height] 
+
+            # Perform element-wise multiplication and sum
+            output[i, j] = np.sum(patch * kernel_rotated[0])
+
+            print("i, j: (", i, j , ")")
+
+
+     
+    
+    #print("dL_dx: After convolution is applied")
+    #print(output)
+    print("--------------") 
+
+
+    """
+    output is dL/dX 
+    """
+    #for i in range(dl_dz_padded_1.shape[0]): #6, 32, 32 #x = 32, 32     
+    #    convolve(dl_dz_padded[i], kernel)
+                                 
+        #    output[] = convolute(patch, kernel)
+
+    #print("dL_dx : after convolution is applied", ) 
+
+    #print("output " , output.shape) 
+
+    return output # this is dL/dx
+ 
